2585f41bd2ce6f80b8622215d1e8c0733a959cde
Samuli Järvensivu <47556122+JarvensivuS@users.noreply.github.com>
2022-09-21 10:29:35 +0300

    Added Tehtävä2

---
+++ Tehtävä2/basicalu.cpp
@@ -0,0 +1,26 @@
+#include "basicalu.h"
+
+BasicALU::BasicALU()
+{
+
+}
+
+double BasicALU::sum(double a, double b)
+{
+    return a+b;
+}
+
+double BasicALU::sub(double a, double b)
+{
+    return a-b;
+}
+
+double BasicALU::mul(double a, double b)
+{
+    return a*b;
+}
+
+double BasicALU::div(double a, double b)
+{
+    return a/b;
+}
---
+++ Tehtävä2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include "calculator.h"
+#include "scientific.h"
+
+int main()
+{
+    // lasketaan perityllä Calculaattorilla
+    Calculator peruslaskin;
+    peruslaskin.inputOperands();
+    peruslaskin.calculateResult();
+    peruslaskin.showResults();
+    cout<<"Ja vaihdetaan laskinta"<<endl;
+
+    // Lasketaan parannetulla Scientific laskimella.
+    Scientific laskin;
+    laskin.inputOperands();
+    laskin.calculateScientific();
+    laskin.showResults();
+
+    return 0;
+}
